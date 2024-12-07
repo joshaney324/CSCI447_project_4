b90f209026963f30d94beb9d346667fe25ffab7f
Owen <ohcool57@gmail.com>
2024-12-06 20:38:53 -0700

    comments for de, pso (and some changes to de)

--- src/DiffEvolution.py
+++ src/DiffEvolution.py
@@ -22,6 +22,7 @@ class DiffEvolution:
 
         self.network = Network(len(hidden_layer_sizes), hidden_layer_sizes, num_inputs, num_outputs, network_type, [])
 
+        # initialize the population
         for i in range(population_size):
             individual = []
             for j in range(len(self.network.layers)):
@@ -29,32 +30,19 @@ class DiffEvolution:
                     node_weight_size = len(self.network.layers[j].node_list[k].weights)
                     for x in range(node_weight_size):
                         individual.append(random.random() * 0.01)
-
+            # add the fitness of the individual to the fitnesses array
             self.population.append(individual)
             self.network.update_weights(individual)
             self.fitnesses.append(self.network.fitness_function(self.fitness_data, self.fitness_labels))
 
-    def mutate(self, base_vector, vector_2, vector_3):
-        donor_vector = []
-        for i in range(len(base_vector)):
-            donor_vector.append(base_vector[i] + self.mutation_rate * (vector_2 - vector_3[i]))
-        return donor_vector
-
-    def cross_over(self, donor_vector, base_vector):
-        offspring_vector = []
-        for i in range(len(donor_vector)):
-            rand_num = random.random()
-            if rand_num <= self.cross_over_rate:
-                offspring_vector.append(base_vector[i])
-            else:
-                offspring_vector.append(donor_vector[i])
-        return offspring_vector
-
+    # cross over and evolve for a single individual in the population -- create an offspring vector and get its fitness
     def evolve_individual(self, base_vector, vector_2, vector_3):
+        # mutate the base vector using vectors two and three to generate the donor vector
         donor_vector = []
         for i in range(len(base_vector)):
-            donor_vector.append(base_vector[i] + self.mutation_rate * (vector_2 - vector_3))
+            donor_vector.append(base_vector[i] + self.mutation_rate * (vector_2[i] - vector_3[i]))
 
+        # cross over with the donor vector generated through mutation
         offspring_vector = []
         for i in range(len(donor_vector)):
             rand_num = random.random()
@@ -63,36 +51,43 @@ class DiffEvolution:
             else:
                 offspring_vector.append(donor_vector[i])
 
+        # get the fitness of the offspring vector
         self.network.update_weights(offspring_vector)
         offspring_fitness = self.network.fitness_function(self.fitness_data, self.fitness_labels)
 
         return (offspring_vector, offspring_fitness)
 
+    # generate the next generation of the population
     def evolve_population(self):
         for i in range(len(self.population)):
             individual = self.population[i]
             fitness = self.fitnesses[i]
 
+            # ensure the trial vectors are distinct
             vector_valid = False
             while not vector_valid:
-                vector_2 = random.randint(1, self.population_size) - 1
-                if vector_2 != individual:
+                vector_2_index = random.randint(1, self.population_size) - 1
+                vector_2 = np.array(self.population[vector_2_index])
+                if not np.array_equal(individual, vector_2):
                     vector_valid = True
             vector_valid = False
             while not vector_valid:
-                vector_3 = random.randint(1, self.population_size) - 1
-                if vector_3 != individual and vector_3 != vector_2:
+                vector_3_index = random.randint(1, self.population_size) - 1
+                vector_3 = np.array(self.population[vector_3_index])
+                if not np.array_equal(individual, vector_3) and not np.array_equal(vector_2, vector_3):
                     vector_valid = True
 
+            # get the offspring of the current individual
             offspring_vector, offspring_fitness = self.evolve_individual(individual, vector_2, vector_3)
 
+            # update the current individual to its offspring if the offspring performs better than the current vector
             if offspring_fitness < fitness:
                 self.population[i] = offspring_vector
                 self.fitnesses[i] = offspring_fitness
 
+    # evolve for the specified number of generations
     def train(self, max_iterations):
         for i in range(max_iterations):
             self.evolve_population()
-
         best_index = np.argmin(self.fitnesses)
         return self.population[best_index]
--- src/ParticleSwarm.py
+++ src/ParticleSwarm.py
@@ -28,6 +28,7 @@ class ParticleSwarm:
 
         self.global_best_position = []
         self.global_best_fitness = np.inf
+        # initialize a weight vector for each member of the population
         for i in range(population_size):
             position = []
             for j in range(len(self.network.layers)):
@@ -35,28 +36,36 @@ class ParticleSwarm:
                     node_weight_size = len(self.network.layers[j].node_list[k].weights)
                     for x in range(node_weight_size):
                         position.append(random.random() * 0.01)
-
+            # check the fitness of the given weight vector
             self.network.update_weights(position)
             fitness = self.network.fitness_function(self.fitness_data, self.fitness_labels)
+            # create a particle with the given weight vector and add it to the population
             particle = Particle(position, fitness, self.max_velocity, self.min_velocity, inertia, personal_weight, global_weight)
             self.population.append(particle)
+            # if the fitness of this weight vector is better than the global best, set it as the global best position
             if fitness < self.global_best_fitness:
                 self.global_best_position = position
                 self.global_best_fitness = fitness
 
-
+    # do one iteration of training
     def update(self):
+        # for every particle in the population...
         for i in range(len(self.population)):
+            # update the velocity and position of the given particle
             self.population[i].update_velocity(self.global_best_position)
             self.population[i].update_position()
+            # check the fitness of the given particle
             self.network.update_weights(self.population[i].position)
             fitness = self.network.fitness_function(self.fitness_data, self.fitness_labels)
+            # if this fitness is better than the particle's personal best, set the personal best
             if fitness < self.population[i].best_fitness:
                 self.population[i].update_best(self.population[i].position, fitness)
+            # if this fitness is better than the global best, set the global best
             if fitness < self.global_best_fitness:
                 self.global_best_position = self.population[i].position
                 self.global_best_fitness = fitness
 
+    # train for the specified number of iterations
     def train(self, max_iterations):
         for i in range(max_iterations):
             self.update()
@@ -75,21 +84,27 @@ class Particle:
         self.global_weight = global_weight
         self.velocity = np.random.uniform(self.min_velocity, self.max_velocity) * 0.5
 
+    # update the best position and fitness value for the particle
     def update_best(self, best_position, best_fitness):
         self.best_position = best_position
         self.best_fitness = best_fitness
 
+    # update the position of the particle
     def update_position(self):
         self.position = self.position + self.velocity
 
+    # update the velocity of the particle
     def update_velocity(self, global_best):
+        # initialize random values
         personal_rand = random.random()
         global_rand = random.random()
         position = np.array(self.position)
         velocity = np.array(self.velocity)
         personal_best = np.array(self.best_position)
         global_best = np.array(global_best)
+        # update the velocity of the particle
         self.velocity = (self.inertia * velocity) + (personal_rand * self.personal_weight * (personal_best - position)) + (global_rand * self.global_weight * (global_best - position))
+        # limit the velocity values to the range specified by min and max velocity
         for i in range(len(self.velocity)):
             if self.velocity[i] < self.min_velocity[i]:
                 self.velocity[i] = self.min_velocity[i]
